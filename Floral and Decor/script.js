623cff05d4198bfb5bf39c2537b770b7628dfc5b
mngoc24 <m1nhn9oc24@gmail.com>
2024-08-21 17:03:59 +0700

    first commit

---
+++ Floral and Decor/script.js
@@ -0,0 +1,16 @@
+
+const accordionButtons = document.querySelectorAll('.accordion-button');
+
+accordionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+
+        button.classList.toggle('active');
+
+        if (button.classList.contains('active')) {
+            content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+            content.style.maxHeight = 0;
+        }
+    });
+});
